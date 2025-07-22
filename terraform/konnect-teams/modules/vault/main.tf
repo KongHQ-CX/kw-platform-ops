a87b9b60a5ea1dd8b2a2d94b00d1d671dfc9630e
pantsel <panagis.tselentis@konghq.com>
2025-07-22 21:13:12 +0200

    Initial setup

--- terraform/konnect-teams/modules/vault/main.tf
+++ terraform/konnect-teams/modules/vault/main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 }
 
-data "vault_auth_backend" "this" {
-  path = "github"
-}
+# data "vault_auth_backend" "this" {
+#   path = "github"
+# }
 
 # Create a team vault mount for the KV version 2 secret engine
 resource "vault_mount" "this" {
@@ -19,28 +19,28 @@ resource "vault_mount" "this" {
   description = "Vault mount for the ${var.team_name} team"
 }
 
-# Create team vault policy
-resource "vault_policy" "this" {
-  name = "${vault_mount.this.path}-policy"
+# # Create team vault policy
+# resource "vault_policy" "this" {
+#   name = "${vault_mount.this.path}-policy"
 
-  policy = <<EOT
-path "${vault_mount.this.path}/data/*" {
-  capabilities = ["read"]
-}
+#   policy = <<EOT
+# path "${vault_mount.this.path}/data/*" {
+#   capabilities = ["read"]
+# }
 
-path "${vault_mount.this.path}/metadata/*" {
-  capabilities = ["read"]
-}
+# path "${vault_mount.this.path}/metadata/*" {
+#   capabilities = ["read"]
+# }
 
-EOT
-}
+# EOT
+# }
 
-# Map policy to team
-resource "vault_github_team" "this" {
-  backend  = data.vault_auth_backend.this.id
-  team     = var.team_name
-  policies = ["${vault_policy.this.name}"]
-}
+# # Map policy to team
+# resource "vault_github_team" "this" {
+#   backend  = data.vault_auth_backend.this.id
+#   team     = var.team_name
+#   policies = ["${vault_policy.this.name}"]
+# }
 
 
 # Store the access token in the KV

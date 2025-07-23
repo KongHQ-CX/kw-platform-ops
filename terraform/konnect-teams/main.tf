cb52041f57f89a009ba4781b546620e852f50f37
pantsel <panagis.tselentis@konghq.com>
2025-07-23 12:55:56 +0200

    Initial setup

--- terraform/konnect-teams/main.tf
+++ terraform/konnect-teams/main.tf
@@ -13,6 +13,11 @@ locals {
   sanitized_team_names = { for team in local.teams : team.name => replace(lower(team.name), " ", "-") }
 }
 
+
+################################################################################
+# STEP 1: CREATE THE KONNECT TEAMS
+################################################################################
+
 resource "konnect_team" "this" {
   for_each = { for team in local.teams : team.name => team }
 
@@ -23,6 +28,10 @@ resource "konnect_team" "this" {
   name = each.value.name
 }
 
+
+################################################################################
+# STEP 2: CREATE THE KONNECT SYSTEM ACCOUNTS FOR EACH TEAM
+################################################################################
 module "system-account" {
   for_each = { for team in konnect_team.this : team.name => team }
 
@@ -33,6 +42,16 @@ module "system-account" {
   team_id           = each.value.id
 }
 
+
+################################################################################
+# STEP 3: CREATE THE TEAMS GITHUB REPOSITORIES
+# (Not in Scope for the Demo)
+################################################################################
+
+
+#########################################################################################
+# STEP 4: CREATE THE VAULT INTEGRATIONS FOR EACH TEAM AND STORE THE SYSTEM ACCOUNT TOKENS
+#########################################################################################
 module "vault" {
   for_each = { for team in konnect_team.this : team.name => team }
 

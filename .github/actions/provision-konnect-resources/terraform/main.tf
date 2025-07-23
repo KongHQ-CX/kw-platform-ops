09df7f972278529f9ba7e3dde3412a19185f3728
pantsel <panagis.tselentis@konghq.com>
2025-07-23 23:02:19 +0200

    try api creation

--- .github/actions/provision-konnect-resources/terraform/main.tf
+++ .github/actions/provision-konnect-resources/terraform/main.tf
@@ -67,7 +67,8 @@ module "apis" {
   description  = lookup(each.value, "description", null)
   labels       = lookup(each.value, "labels", {})
   slug         = lookup(each.value, "slug", null)
-  spec_content = lookup(each.value, "spec_content", null)
+  # spec_content = lookup(each.value, "spec_content", null)
+  spec_content = yamldecode(file("../../${each.value.spec_content.file}"))
   api_version  = lookup(each.value, "version", null)
 }
 

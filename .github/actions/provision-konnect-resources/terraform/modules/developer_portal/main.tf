25dfe7ce8d4714c91b4b9abab742104588c0c310
battlebyte <jordi@fernandez.name>
2025-09-17 12:54:33 +0200

    APIOps 201 preparation

--- .github/actions/provision-konnect-resources/terraform/modules/developer_portal/main.tf
+++ .github/actions/provision-konnect-resources/terraform/modules/developer_portal/main.tf
@@ -8,17 +8,16 @@ terraform {
 }
 
 resource "konnect_portal" "this" {
-  name        = var.name
-  description = var.description
-  labels      = var.labels
-}
-
-output "id" {
-  value       = konnect_portal.this.id
-  description = "Portal ID"
-}
-
-output "name" {
-  value       = konnect_portal.this.name
-  description = "Portal name"
+  name                                 = var.name
+  description                          = var.description
+  display_name                         = var.display_name
+  labels                               = var.labels
+  authentication_enabled               = var.authentication_enabled
+  auto_approve_applications            = var.auto_approve_applications
+  auto_approve_developers              = var.auto_approve_developers
+  default_api_visibility               = var.default_api_visibility
+  default_application_auth_strategy_id = var.default_application_auth_strategy_id
+  default_page_visibility              = var.default_page_visibility
+  rbac_enabled                         = var.rbac_enabled
+  force_destroy                        = var.force_destroy
 }
